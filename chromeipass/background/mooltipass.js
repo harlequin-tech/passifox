dd20ced3d048d790612430d8fb26d1425824febd
Harlequin <harlequin@hunt.net.nz>
2014-09-19 00:16:24 +1200

    Remove CDDL license header for now

--- chromeipass/background/mooltipass.js
+++ chromeipass/background/mooltipass.js
@@ -1,39 +1,3 @@
-/* CDDL HEADER START
- *
- * The contents of this file are subject to the terms of the
- * Common Development and Distribution License (the "License").
- * You may not use this file except in compliance with the License.
- *
- * You can obtain a copy of the license at src/license_cddl-1.0.txt
- * or http://www.opensolaris.org/os/licensing.
- * See the License for the specific language governing permissions
- * and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL HEADER in each
- * file and include the License file at src/license_cddl-1.0.txt
- * If applicable, add the following below this CDDL HEADER, with the
- * fields enclosed by brackets "[]" replaced with your own identifying
- * information: Portions Copyright [yyyy] [name of copyright owner]
- *
- * CDDL HEADER END
- */
-
-/* Copyright (c) 2014 Darran Hunt. All rights reserved. */
-
-/*!      \file background.js
-*        \brief        Mooltipass Chrome Authentication plugin
-*        Created: 14/6/2014
-*        Author: Darran Hunt
-*
-*        Waits for requests from the web page content script for
-*        credentials, asks the Mooltipass Client for them, then sends
-*        the response back to the content script.
-* 
-* 	Modified: 12/09/2014 by Bjorn Wielens
-* 	- Implemented blacklist for sites.
-* 	- Implemented card presence detection.
-*/
-
 
 var mooltipass = {};
 var mpClient = null;
